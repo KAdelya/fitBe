d49801aa064b4ac3cebca0b32eb109a2361dd5c1
Adelya <adelya6387240@gmail.com>
2022-03-05 10:36:34 +0300

    Add cpmponent for svg

--- app/src/components/MainBackground/MainBackground.tsx
+++ app/src/components/MainBackground/MainBackground.tsx
@@ -1,6 +1,6 @@
 import styles from './MainBackground.module.sass'
 
-export const MainBackground = () => {
+export const MainBackground = (props:any) => {
     return (
         <div className={styles.main_background}></div>
     );
--- app/src/pages/Advertising/Advertising.tsx
+++ app/src/pages/Advertising/Advertising.tsx
@@ -1,8 +1,17 @@
+import { Icon } from '../../components/Icon/Icon';
 import { MainBackground } from '../../components/MainBackground/MainBackground';
 import styles from './Advertising.module.sass'
 
 export const Advertising = () => {
     return (
-        <MainBackground/>
+        <div>
+            <MainBackground/>
+            <header>
+                <div className={styles.logo_image}>
+                    <Icon name="logo" width="112" height="88"/>
+                    <div className={styles.logo_name}>BeFit</div>
+                </div>
+            </header>
+        </div>
     );
 }
