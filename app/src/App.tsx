ed8e38efa3483c125ef0dee85f1a6744b3e9b0bb
Adelya <adelya6387240@gmail.com>
2022-03-12 17:54:55 +0300

    Add RegistrationPage

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { Advertising } from './pages/Advertising/Advertising';
 import styles from './App.module.sass'
 import { MainPage } from './pages/MainPage/MainPage';
+import { Registration } from './pages/Registration/Registration'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.main_content}>
-      <MainPage />
+        <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<MainPage />}/>
+            <Route path='/registration' element={<Registration/>}/>
+        </Routes>
+        </BrowserRouter>
       {/* <Advertising /> */}
     </div>
   );
