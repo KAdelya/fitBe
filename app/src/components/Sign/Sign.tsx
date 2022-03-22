c0bcf25fd28779c500f75859fb684d7d467e54d1
Adelya <adelya6387240@gmail.com>
2022-03-22 12:09:28 +0300

    Add components

---
+++ app/src/components/Sign/Sign.tsx
@@ -0,0 +1,18 @@
+import styles from './Sign.module.sass';
+import time from "../../assets/images/time.svg";
+
+export const Sign = () => {
+    return (
+        <div className={styles.input_and_button_wrapper}>
+            <div className={styles.login_statement}>Время тренироваться!</div>
+            <div className={styles.time}>
+                <img src={time} />
+            </div>
+            <input placeholder='Имя/Email'/>
+            <input placeholder='Пароль' />
+            <div className={styles.button_wrapper}>
+                <button>Войти</button>
+            </div>
+        </div>
+    );
+}
--- app/src/pages/SignPage/SignPage.tsx
+++ app/src/pages/SignPage/SignPage.tsx
@@ -1,6 +1,7 @@
 import styles from "../SignPage/SignPage.module.sass";
 import time from "../../assets/images/time.svg";
 import {HeaderSignAndRegistration} from "../../components/HeaderSignAndRegisteration/HeaderSignAndRegistration";
+import {Sign} from "../../components/Sign/Sign";
 
 export const SignPage = () => {
     return (
@@ -8,17 +9,7 @@ export const SignPage = () => {
             <div className={styles.page_content_wrapper}>
                 <div className={styles.upper_content_wrapper}>
                     <HeaderSignAndRegistration/>
-                    <div className={styles.input_and_button_wrapper}>
-                        <div className={styles.login_statement}>Время тренироваться!</div>
-                        <div className={styles.time}>
-                            <img src={time} />
-                        </div>
-                        <input placeholder='Имя/Email'/>
-                        <input placeholder='Пароль' />
-                        <div className={styles.button_wrapper}>
-                            <button>Войти</button>
-                        </div>
-                    </div>
+                    <Sign/>
                 </div>
             </div>
         </div>
