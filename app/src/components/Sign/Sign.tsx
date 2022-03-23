dbe684e134e368e870de7eaac9f2a3d350696cfb
Adelya <adelya6387240@gmail.com>
2022-03-23 16:31:11 +0300

    Add finished validation on signPage

--- app/src/components/Sign/Sign.tsx
+++ app/src/components/Sign/Sign.tsx
@@ -1,18 +1,56 @@
 import styles from './Sign.module.sass';
 import time from "../../assets/images/time.svg";
+import * as yup from "yup";
+import {Formik} from 'formik';
 
 export const Sign = () => {
+    const validationsSchema = yup.object().shape({
+        email: yup.string().typeError('Должно быть строкой').required('Обязательно')
+            .matches(/^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/, 'Введите верный email'),
+        password: yup.string().typeError('Должно быть строкой').required('Введите верные данные')
+            .matches(/[0-9a-zA-Z]{4,}/g, 'Пароль должен состоять из минимум 4 цифр или латинских букв')
+    })
     return (
-        <div className={styles.input_and_button_wrapper}>
-            <div className={styles.login_statement}>Время тренироваться!</div>
-            <div className={styles.time}>
-                <img src={time} />
-            </div>
-            <input placeholder='Имя/Email'/>
-            <input placeholder='Пароль' type='password'/>
-            <div className={styles.button_wrapper}>
-                <button>Войти</button>
-            </div>
+        <div>
+            <Formik
+                initialValues={{
+                    name: '',
+                    email: '',
+                    password: '',
+                    repeat_password: ''
+                }}
+                onSubmit={(values) => {
+                    console.log(values)
+                }}
+                validationSchema={validationsSchema}
+            >
+                {({
+                      values, errors, touched,
+                      handleChange, handleBlur,
+                      isValid = false, dirty = false, handleSubmit
+                  }) => (
+                    <form onSubmit={handleSubmit}>
+                        <div className={styles.input_and_button_wrapper}>
+                            <div className={styles.login_statement}>Время тренироваться!</div>
+                            <div className={styles.time}>
+                                <img src={time}/>
+                            </div>
+                            <input placeholder='Email' type='email' name={`email`}
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
+                                   value={values.email}/>
+                            {touched.email && errors.email && <p style={{'color': 'red'}}>{errors.email}</p>}
+                            <input placeholder='Пароль' type='password' name={"password"}
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
+                                   value={values.password}/>
+                            {touched.password && errors.password && <p style={{'color': 'red'}}>{errors.password}</p>}
+                            <div className={styles.button_wrapper}>
+                                <button type={`submit`} disabled={!(isValid || dirty)}>Войти</button>
+                            </div>
+                        </div>
+                    </form>)}
+            </Formik>
         </div>
     );
 }
