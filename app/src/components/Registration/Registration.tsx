1e19f4db137fc64a423cc467cd7e42c877e8d920
Adelya <adelya6387240@gmail.com>
2022-03-27 21:17:45 +0300

    Add connection firebase

--- app/src/components/Registration/Registration.tsx
+++ app/src/components/Registration/Registration.tsx
@@ -1,9 +1,20 @@
 import styles from './Registration.module.sass';
 import {Formik} from 'formik';
 import * as yup from 'yup';
-import {useState} from "react";
+import React, {useState} from "react";
+import { getDatabase, ref, set } from "firebase/database";
 
 export const Registration = () => {
+    // let textInput = React.createRef()
+    function writeUserData(userId='1') {
+        const db = getDatabase();
+        set(ref(db, 'users/' + userId), {
+            name: 'Adelya',
+            email: 'dfv2@mail.ru',
+            password: '0900',
+            repeat_password: '0900'
+        });
+    }
     const [correctPassw, setCorrectPassw] = useState(false)
     const validationsSchema = yup.object().shape({
         name: yup.string().typeError('Должно быть строкой').required('Обязательно')
@@ -45,25 +56,27 @@ export const Registration = () => {
                     <form onSubmit={handleSubmit}>
                         <div className={styles.input_and_button_wrapper}>
                             <div className={styles.question_register}>Зарегистрируемся?</div>
-                            <input placeholder='Email' type='email' name={`email`}
+                            <input placeholder='Email' type='email' name={`email`} id='email'
                                    onChange={handleChange}
                                    onBlur={handleBlur}
-                                   value={values.email}/>
+                                   value={values.email}
+                            onClick={handleChange}/>
                             {touched.email && errors.email && <p style={{'color': 'red'}}>{errors.email}</p>}
-                            <input placeholder='Имя' type='name' name={`name`}
+                            <input placeholder='Имя' type='name' name={`name`} id='name'
                                    onChange={handleChange}
                                    onBlur={handleBlur}
-                                   value={values.name}/>
+                                   value={values.name} onClick={handleChange}/>
                             {touched.name && errors.name && <p style={{'color': 'red'}}>{errors.name}</p>}
-                            <input placeholder='Пароль' type='password' name={"password"}
+                            <input placeholder='Пароль' type='password' name={"password"} id='password'
                                    onChange={handleChange}
                                    onBlur={handleBlur}
-                                   value={values.password}/>
+                                   value={values.password} onClick={handleChange}/>
                             {touched.password && errors.password && <p style={{'color': 'red'}}>{errors.password}</p>}
                             <input placeholder='Подтверждение пароля' type='password' name={"repeat_password"}
+                                   id='repeat_password'
                                    onChange={handleChange}
                                    onBlur={handleBlur}
-                                   value={values.repeat_password}/>
+                                   value={values.repeat_password} onClick={handleChange}/>
                             {touched.repeat_password && errors.repeat_password &&
                             <p style={{'color': 'red'}}>{errors.repeat_password}</p>}
                             <div className={styles.button_wrapper}>
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -6,6 +6,20 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'mobx-react';
 import mainStore from './stores/mainStore';
 import {ModalConstructor} from "./components/modal/ModalConstructor";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCPE_phxJ_Xbg6P-FEWVzH6IirTVpd784g",
+    authDomain: "fitbe-f03ed.firebaseapp.com",
+    projectId: "fitbe-f03ed",
+    storageBucket: "fitbe-f03ed.appspot.com",
+    messagingSenderId: "1078037220285",
+    appId: "1:1078037220285:web:f394833d5670da486860d4",
+    measurementId: "G-ECNRVXXWZX"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
