fac7df94b0716ec2530884074aa2bead25e28e33
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-05-06 11:57:48 +0300

    added constants

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,41 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.module.sass';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'mobx-react';
+import { Provider } from 'react-redux';
 import mainStore from './stores/mainStore';
 import { ModalConstructor } from "./components/Modal/ModalConstructor";
 import { getAuth } from 'firebase/auth'
 import firebase, { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database';
-
+import { store } from './stores/slices';
+import  * as constants from './constants';
 
 
 const app = initializeApp({
-  // apiKey: "AIzaSyATktBg4k_tRIMEAEuVlskz7lNu8gEvD6c",
-  // authDomain: "fitbev2.firebaseapp.com",
-  // databaseURL: "https://fitbev2-default-rtdb.firebaseio.com",
-  // projectId: "fitbev2",
-  // storageBucket: "fitbev2.appspot.com",
-  // messagingSenderId: "1020844677779",
-  // appId: "1:1020844677779:web:054ad5b5bda974ce948b66",
-  // measurementId: "G-DV3TQE5BE0"
-  apiKey: "AIzaSyA0fvCh-aT77nvqfLSgHlCpGM_wcbNzTOI",
-  authDomain: "befit-ef9f9.firebaseapp.com",
-  databaseURL: "https://befit-ef9f9-default-rtdb.firebaseio.com",
-  projectId: "befit-ef9f9",
-  storageBucket: "befit-ef9f9.appspot.com",
-  messagingSenderId: "1001074447643",
-  appId: "1:1001074447643:web:09a66c1769f37aa2676fbe"
+  apiKey: constants.REACT_APP_FIREBASE_API_KEY,
+  authDomain: constants.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: constants.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: constants.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: constants.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: constants.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ,
+  appId: constants.REACT_APP_FIREBASE_APP_ID
 });
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider {...mainStore}>
+    {/* <Provider {...mainStore}> */}
+    <Provider store={store}>
       <App />
       <ModalConstructor />
-    </Provider>
+      </Provider>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
