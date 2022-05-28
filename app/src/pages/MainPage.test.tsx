2ed69af9a66655f66e34e792b42ff4acbf661aff
Adelya <adelya6387240@gmail.com>
2022-05-28 12:57:07 +0300

    Add new tests

--- app/src/pages/MainPage.test.tsx
+++ app/src/pages/MainPage.test.tsx
@@ -1,30 +1,52 @@
 import {render, screen} from "@testing-library/react";
 import {MainPage} from "./Main/MainPage";
 import {Routes, BrowserRouter as Router, Route} from "react-router-dom";
-import styles from "../App.module.sass";
 import store, {persistor} from "../redux";
-import ReactDOM from "react-dom";
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 import React from 'react';
 
-describe("when rendered with a `name` prop", () => {
-    it("should paste it into the greetings text", () => {
+describe("when rendered MainPage", () => {
+    it("checking that the some texts are displayed", () => {
         render(
             <React.StrictMode>
                 <Provider store={store}>
                     <PersistGate loading={null} persistor={persistor}>
-            <Router>
-                <Routes>
-                    <Route path='/' element={<MainPage  />} />
-                </Routes>
-            </Router>
+                        <Router>
+                            <Routes>
+                                <Route path='/' element={<MainPage/>}/>
+                            </Routes>
+                        </Router>
                     </PersistGate>
                 </Provider>
             </React.StrictMode>
         );
         expect(
-            screen.getByText(/We encourage you to go on a fitness journey with our app!/)
+            screen.getByText('Making health a priority with BeFit')
         ).toBeInTheDocument();
+        expect(
+            screen.getByText('Who We Are?')
+        ).toBeInTheDocument();
+    });
+    it("checking that the some buttons are displayed", () => {
+        render(
+            <React.StrictMode>
+                <Provider store={store}>
+                    <PersistGate loading={null} persistor={persistor}>
+                        <Router>
+                            <Routes>
+                                <Route path='/' element={<MainPage/>}/>
+                            </Routes>
+                        </Router>
+                    </PersistGate>
+                </Provider>
+            </React.StrictMode>
+        );
+        expect(
+            screen.getAllByRole("button",  {name: "REGISTRATION"})
+        );
+        expect(
+            screen.getAllByRole("button",  {name: "SIGN IN"})
+        );
     });
 });
