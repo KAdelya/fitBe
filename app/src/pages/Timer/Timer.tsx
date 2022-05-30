2b43ccdf1c24465d43a7d7d001cd440dd02f92f5
Adelya <adelya6387240@gmail.com>
2022-05-30 15:07:58 +0300

    Add fix timer

--- app/src/pages/Timer/Timer.tsx
+++ app/src/pages/Timer/Timer.tsx
@@ -2,18 +2,19 @@ import styles from './Timer.module.sass';
 import line from '../../assets/images/line.svg';
 import arr from '../../assets/images/arr.svg';
 import rarr from '../../assets/images/rarr.svg';
-import { ModalTimer } from '../../components/Modal/ModalTimer';
+import {ModalTimer} from '../../components/Modal/ModalTimer';
 import MainCustomBtn from '../../components/ui/button/ButtonLayout/ButtonLayout';
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import ModalLayout from '../../components/Containers/ModalContainer/ModalContainer';
-import { useAppDispatch, useAppSelector } from '../../utils/redux-hooks';
-import { setModal } from '../../redux/slices/modalSlice';
+import {useAppDispatch, useAppSelector} from '../../utils/redux-hooks';
+import {setModal} from '../../redux/slices/modalSlice';
 import CustomBtnLayout from '../../components/ui/button/CustomBtnLayout/CustomBtnLayout';
+import {setRoundsCount} from "../../redux/slices/timerSlice";
 
 const Timer = () => {
     const [time, setTime] = useState(0);
     const [timeOn, setTimeOn] = useState(false);
-    
+    const [currentRound, setCurrentRound] = useState(0);
     useEffect(() => {
         let interval: any = null;
         if (timeOn) {
@@ -27,7 +28,16 @@ const Timer = () => {
     }, [timeOn]);
 
     const [visible, setVisible] = useState(false);
-
+    const changeRoundLeft = () => {
+        if (currentRound > 0) {
+            setCurrentRound(currentRound - 1);
+        }
+    };
+    const changeRoundRight = () => {
+        if (currentRound < roundsCount) {
+            setCurrentRound(currentRound + 1);
+        }
+    };
     const show = useAppSelector((state) => state.modal.show);
     const dispatch = useAppDispatch();
     const handleClose = () => {
@@ -37,14 +47,14 @@ const Timer = () => {
             })
         );
     };
-    const { workTime, roundsCount } = useAppSelector(state => state.timer);
+    const {workTime, roundsCount} = useAppSelector(state => state.timer);
     return (
         <div>
 
             <section className={styles.timer_page}>
                 <div className={styles.timer_page__content}>
                     <div className={styles.timer_page__content__time}>
-                        <p>{Math.floor((time / 6000) % 60) < roundsCount ? Math.floor((time / 100) % 60) : 0}</p>
+                        <p>{Math.floor((time / 6000) % workTime) < roundsCount ? Math.floor((time / 100) % workTime) : time}</p>
                     </div>
                     <CustomBtnLayout>
                         <button onClick={() => setVisible(!visible)}>SET THE TIME</button>
@@ -58,12 +68,15 @@ const Timer = () => {
                         </button>
                     </div>
                     <div className={styles.timer_page__info__set}>
-                        <img src={line} width={5} alt='arrow' />
-                        <img src={rarr} width={35} alt='arrow' />
-                        <p> {Math.floor((time / 6000) % 60) < roundsCount ? Math.floor((time / 6000) % 60) : roundsCount}
-                            /{roundsCount}</p>
-                        <img src={arr} width={35} alt='arrow' />
-                        <img src={line} width={5} alt='arrow' />
+                        <button onClick={changeRoundLeft}>
+                            <img src={line} width={5} alt='arrow'/>
+                            <img src={rarr} width={35} alt='arrow'/>
+                        </button>
+                        <p> {currentRound}/{roundsCount}</p>
+                        <button onClick={changeRoundRight}>
+                        <img src={arr} width={35} alt='arrow'/>
+                        <img src={line} width={5} alt='arrow'/>
+                        </button>
                     </div>
                     <div className={styles.timer_page__info__bottom}>
                         <MainCustomBtn>
@@ -83,7 +96,7 @@ const Timer = () => {
                     close={handleClose}
                     open={show}
                     button="START">
-                    <ModalTimer />
+                    <ModalTimer/>
                 </ModalLayout>
                 :
                 <></>}
