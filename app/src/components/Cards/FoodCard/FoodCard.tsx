ae1a503a7b964bf4ccdf11120899bf01b9a73b19
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-05-30 10:33:54 +0300

    Revert "Merge branch 'main' of https://github.com/KAdelya/fitBe"
    This reverts commit 54b6e85584afaa9c0c9b1d01b0755058a4fb9798, reversing
    changes made to 6dba834556f97f5b541531f8bfd0c7f6b95f4422.

--- app/src/components/Cards/FoodCard/FoodCard.tsx
+++ app/src/components/Cards/FoodCard/FoodCard.tsx
@@ -25,21 +25,13 @@ const FoodCard: FC<ITitle> = ({ title }) => {
                         fats: (foodItems[i].fats) / 100 * parseFloat(weight),
                         squirrels: (foodItems[i].squirrels) / 100 * parseFloat(weight)
                     }));
-                dispatch(addFoodItem
-                ({
-                    calories: Math.round(foodItems[i].calories/100*parseFloat(weight)),
-                    carbohydrates: Math.round(foodItems[i].carbohydrates/100*parseFloat(weight)),
-                    fats: Math.round(foodItems[i].fats/100*parseFloat(weight)),
-                    squirrels: Math.round(foodItems[i].squirrels/100*parseFloat(weight))
-                }));
                     setName('');
                     setWeight('');
             }
-
             else 
                 setName('');
                 setWeight('');
-
+        };
     };
 
         return (
